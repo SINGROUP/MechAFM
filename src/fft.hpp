a0c83636d07c952430bb8e9fe1c22d9245955b62
Juha Ritala <juha.ritala@aalto.fi>
2016-03-23 09:53:02 +0200

    Changed the FFT normalization so that the transform is unitary.

--- src/fft.hpp
+++ src/fft.hpp
@@ -18,17 +18,20 @@ using namespace std;
 
 typedef complex<double> dcomplex;
 
+
 /** \brief Does FFT on data_grid_in and stores the result to data_grid_out
  * 
  *  data_grid_out is allocated by the function, so you don't need to do that beforehand.
- *  No scaling is done. Automatically changes spacing from real space to k-space.
+ *  The result is scaled using factor 1/sqrt(total_number_of_grid_points). Automatically
+ *  changes spacing from real space to k-space.
  */
 void fft_data_grid(const DataGrid<double>& data_grid_in, DataGrid<dcomplex>& data_grid_out);
+
 
 /** \brief Does inverse FFT on data_grid_in and stores the result to data_grid_out
  * 
  *  data_grid_out is allocated by the function, so you don't need to do that beforehand.
- *  The result is scaled using factor 1/total_number_of_grid_points. Automatically
+ *  The result is scaled using factor 1/sqrt(total_number_of_grid_points). Automatically
  *  changes spacing from k-space to real space.
  */
 void ffti_data_grid(const DataGrid<dcomplex>& data_grid_in, DataGrid<double>& data_grid_out);
